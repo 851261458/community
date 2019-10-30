fc3fdbf5099b789923e21f839cd5be4b7e8c8df2
lwd <851261458@qq.com>
2019-10-30 19:24:03 +0800

    add jquery and bootstrap

---
+++ src/main/java/com/mju/band3/community/Controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.mju.band3.community.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class IndexController {
+    @GetMapping(value = "/")
+    public String Index(){
+        return "index";
+    }
+}
