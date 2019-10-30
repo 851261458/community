f16da251ec4d824cdd53314adb4e4dc0062210ef
lwd <851261458@qq.com>
2019-10-30 14:45:19 +0800

    init repo

---
+++ src/test/java/com/mju/band3/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mju.band3.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
