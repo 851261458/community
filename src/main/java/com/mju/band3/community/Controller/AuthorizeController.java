87dab62fa2a58c8dc34f928d3eebced8f110abb3
lwd <851261458@qq.com>
2019-11-01 18:23:07 +0800

    aotu login by session and cookie

--- src/main/java/com/mju/band3/community/Controller/AuthorizeController.java
+++ src/main/java/com/mju/band3/community/Controller/AuthorizeController.java
@@ -2,12 +2,19 @@ package com.mju.band3.community.Controller;
 
 import com.mju.band3.community.Entity.AccessTokenDTO;
 import com.mju.band3.community.Entity.GitHupUser;
+import com.mju.band3.community.Mapper.UserMapper;
+import com.mju.band3.community.Model.User;
 import com.mju.band3.community.Provider.GitHupProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -19,20 +26,35 @@ public class AuthorizeController {
     String setRedirectUri;
     @Autowired
     GitHupProvider gitHupProvider;
+    @Autowired
+    UserMapper userMapper;
 
     @GetMapping(value = "/callback")
     public String callBack(@RequestParam(name ="code")String code,
-                           @RequestParam(name="state")String state){
+                           @RequestParam(name="state")String state,HttpServletResponse response){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("84f481e2429bb9deb1c3");
-        accessTokenDTO.setClient_secret("d0e1b4793c812438e202b2aad4c4f500ce5b8c71");
+        accessTokenDTO.setClient_id(setClientId);
+        accessTokenDTO.setClient_secret(setClientSecret);
         accessTokenDTO.setCode(code);
         accessTokenDTO.setState(state);
-        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setRedirect_uri(setRedirectUri);
         String accessToken = gitHupProvider.getAccessToken(accessTokenDTO);
-        GitHupUser user = gitHupProvider.getUser(accessToken);
-        System.out.println(user);
-        return "index";
+        GitHupUser gitHupUser = gitHupProvider.getUser(accessToken);
+        if (gitHupUser!=null){
+            User user=new User();
+            String token=UUID.randomUUID().toString();
+            user.setToken(token);
+            user.setName(gitHupUser.getName());
+            user.setAccountId(String.valueOf(gitHupUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            response.addCookie(new Cookie("token",token));
+            return "redirect:/";
+        }else {
+            return "redirect:/";
+        }
+
 
     }
 
